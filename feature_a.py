4a190e32cbb960d93f3299cc4730ec0631ed97db
Martin <vivasdema@gmail.com>
2024-03-02 22:21:27 -0500

    Feature 1 subtask 3

--- feature_a.py
+++ feature_a.py
@@ -1,5 +1,12 @@
 def calculate_average_velocity():
     print("Enter sprint times separated by space (e.g., 140 150 160):")
+    sprint_times = list(map(int, input().split()))
+    # if there is no input
+    if len(sprint_times) < 1:
+        print("Please enter at least one sprint time.")
+    else: 
+        average_velocity = round(sum(sprint_times) / len(sprint_times),1)
+        print(average_velocity)
 
 if __name__ == "__main__":
     calculate_average_velocity()
